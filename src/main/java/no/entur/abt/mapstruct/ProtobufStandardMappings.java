733f015a052f74ae47c12a54f5dd5adf51dd106e
erlend <erlend@scelto.no>
2021-02-01 10:36:43 +0100

    Replace usage of Duration.toNanosPart with getNanos as former is not available in android sdk (v29)

--- src/main/java/no/entur/abt/mapstruct/ProtobufStandardMappings.java
+++ src/main/java/no/entur/abt/mapstruct/ProtobufStandardMappings.java
@@ -77,7 +77,7 @@ public interface ProtobufStandardMappings {
 	}
 
 	default com.google.protobuf.Duration mapDuration(Duration t) {
-		return com.google.protobuf.Duration.newBuilder().setSeconds(t.toMillis() / 1000).setNanos(t.toNanosPart()).build();
+		return com.google.protobuf.Duration.newBuilder().setSeconds(t.toMillis() / 1000).setNanos(t.getNano()).build();
 	}
 
 	default com.google.type.Date mapLocalDate(LocalDate t) {
